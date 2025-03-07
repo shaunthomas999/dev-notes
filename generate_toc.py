1267eb48c5ab5cab13cdffd886c5c31e9ef2c5f9
Shaun Thomas <shaunthomas999@gmail.com>
2025-03-07 10:42:51 +0100

    TOC

--- generate_toc.py
+++ generate_toc.py
@@ -13,13 +13,13 @@ def generate_toc(root_dir):
         if depth == 0:
             toc_lines.append(f'# {os.path.basename(os.path.abspath(dirpath))}\n')
         else:
-            toc_lines.append(f'{indent}- **{os.path.basename(dirpath)}**\n')
+            toc_lines.append(f'{indent}- {os.path.basename(dirpath)}\n')
         subindent = '  ' * (depth + 1)
         for filename in sorted(filenames):
             if filename.endswith('.md'):
                 filepath = os.path.join(dirpath, filename).replace('\\', '/')
-                file_url = f'{repo_url}/{filepath}'
-                toc_lines.append(f'{subindent}- [{filename}]({file_url})\n')
+                file_url = f'{filepath}'
+                toc_lines.append(f'{subindent}- [{filename}](<{file_url}>)\n')
     return toc_lines
 
 if __name__ == '__main__':
